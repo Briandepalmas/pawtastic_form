362448f769bfdaf70f3a6bd5c1424fae04aa56c6
Briandepalmas <Student_GA110@Student-GA110.home>
2020-03-29 21:49:46 -0400

    pasted forms exercise as template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
-import './App.css';
+import './App.css'
 
 export class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={ value:''}
+
+
+    this.handleSubmit=this.handleSubmit.bind(this)
+    this.handleChange=this.handleChange.bind(this)
+  }
+
+  handleChange(event)  {
+this.setState({value: event.target.value})
+  }
+  handleSubmit(event)  {
+alert(`A change happened ${this.state.value}`)
+event.preventDefault()
+  }
+
+
   render() {
     return (
       <div>
+        <form onSubmit={this.handleChange}>
+          <label>
+          Name:
+          <input type="text" onChange={this.handleChange}/>
+          <input type="submit" value="Submit" onClick={this.handleSubmit}/>
+        </label>
+        </form>
         
       </div>
     )
